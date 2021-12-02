31aa30a753a17c401dd8de26b37fe902c8d5969d
brigitte.nikoll <roadclosed24@gmail.com>
2021-12-01 19:01:35 -0500

    Mejorando layout (queda pendiente mejorar cards)

--- poke-api/app.js
+++ poke-api/app.js
@@ -1,4 +1,3 @@
-
 const container = document.getElementById("container");
 const fetching = async () => {
   for (let i = 1; i <= 40; i++) {
@@ -10,9 +9,8 @@ const gettingData = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const pokemon = await res.json();
-  console.log(pokemon)
+  console.log(pokemon);
   pokecard(pokemon);
-
 };
 
 fetching();
@@ -21,27 +19,32 @@ function pokecard(pokemon) {
   let containCars = document.createElement("div");
   containCars.classList.add("pokemon");
   containCars.innerHTML = `
-  <div class="pkm-card">${pokemon.name}
-  <img class="pkm-images" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
-  
-  
+  <div class="pkm-card">
+    <div>${pokemon.id}</div>
+    <div>${pokemon.name}</div>
+    <div class="type"> ${pokemon.types[0].type.name}</div>
+    <img class="pkm-images" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
   </div>
     `;
-  containCars.addEventListener('click', () => {
-    console.log("hola")
+  containCars.addEventListener("click", () => {
+    console.log("hola");
     let open = document.querySelector(".modalBackground");
-      let openContain = document.querySelector(".modal");
-      open.style.visibility = "visible";
-      open.style.opacity = "1";
-      openContain.style.visibility = "visible";
-      openContain.style.opacity = "1";
-      document.querySelector(".modal").innerHTML = "";
-      document.querySelector(".modal").innerHTML += `
-      <div class="pkm-card">${pokemon.id}
+    let openContain = document.querySelector(".modal");
+    open.style.visibility = "visible";
+    open.style.opacity = "1";
+    openContain.style.visibility = "visible";
+    openContain.style.opacity = "1";
+    document.querySelector(".modal").innerHTML = "";
+    document.querySelector(".modal").innerHTML += `
+      <div class="pkm-modal">
+        <div>
+          
+        </div>
+        <div>${pokemon.name}</div>
      
-      <div class="type"> ${pokemon.types[0].type.name}</div>
-      </div>`
-  })
+        
+      </div>`;
+  });
   let closeModal = document.getElementById("close");
   closeModal.addEventListener("click", () => {
     let close = document.querySelector(".modalBackground");
