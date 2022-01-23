4dd27658a694a685f1a77fb05d9e04e19132ed5e
brigitte.nikoll <roadclosed24@gmail.com>
2022-01-23 11:41:27 -0500

    Se agregó una función para disminuir al ejemplo de useState

--- product-list-react/my-list/src/UseStateExplame.jsx
+++ product-list-react/my-list/src/UseStateExplame.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 
 function UseStateExplame() {
   const [counter, setCounter] = useState(0);
-  function activador() {
+  function more() {
     setCounter(counter + 1);
+  }
+  function less() {
+    setCounter(counter - 1);
   }
 
   return (
     <div>
       <h1>Este es un contador: ${counter}</h1>
-      <button onClick={activador}>Click</button>
+      <button onClick={more}>Aumentar</button>
+      <button onClick={less}>Disminuir</button>
     </div>
   );
 }
