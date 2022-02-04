6a205df1a53524f361d3ed333f4b8abcb8d6f533
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-04 18:47:28 -0500

    Adding Assement-I

--- games/games/src/App.js
+++ games/games/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const emojipedia = {
+    platano: '🍌',
+    manzana: '🍎'
+
+  }
+
+  const [text, setText] = useState("Este es un texto");
+
   return (
     <div className="App">
-      
+      <textarea textarea cols="30" rows="10"></textarea>
+      <p>{text}</p>
+      <p></p>
     </div>
   );
-}
+};
 
 export default App;
