21ef06c451ca0b5cec6930655e1eac6ee69c3785
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-11 01:08:13 -0500

    Mostrando gif con axios

--- games/gift-game/my-gift-app/src/pages/GifGenerator.jsx
+++ games/gift-game/my-gift-app/src/pages/GifGenerator.jsx
@@ -1,31 +1,44 @@
 import { useState } from "react";
+import axios from "axios";
 import "./_GifGenerator.css";
 
 const GitGenerator = () => {
-  const [query, setQuery] = useState("hola");
+  const [query, setquery] = useState("hola");
+  const [gifs, setgifs] = useState([]);
+
+  const handleGifs = async () => {
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        api_key: process.env.REACT_APP_GIPHY_KEY_NAME,
+        q: query,
+        limit: 10,
+      },
+    });
+    console.log("query", query);
+    console.log("response", response.data.data);
+    setgifs(response.data.data);
+  };
 
   return (
     <div className="gifGenerator">
       <div className="gifGenerator__search">
-        <input type="text" onChange={(e) => setQuery(e.target.value)} />
-        <button onClick={() => console.log(query)}>New Gifs</button>
+        <input type="text" onChange={(e) => setquery(e.target.value)} />
+        <button onClick={handleGifs}>New Gifs</button>
       </div>
 
-      <div className="gifs"></div>
+      <div className="gifs">
+        {gifs.map(({ images }) => {
+          return (
+            <img
+              src={images.fixed_height_small.url}
+              alt={images.fixed_height_small.url}
+              key={images.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
 export default GitGenerator;
-
-/*   const contador = () => {
-    const [counter, setCounter] = useState(0);
-  
-    return (
-      <div className="gifGenerator">
-        <p>Contador : {counter}</p>
-  
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-      </div>
-    );
-  }; */
