796ee0ab5368bad596e36a78984b9289423d7a5b
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-13 12:54:36 -0500

    Adding button to change a color selector

--- games/gift-game/my-gift-app/src/components/shape/Shape.jsx
+++ games/gift-game/my-gift-app/src/components/shape/Shape.jsx
@@ -1,28 +1,20 @@
 import { useState } from "react";
 import "./_Shape.css";
 
-const Shape = ({ type }) => {
+const Shape = ({ type, userColor }) => {
   const [bgColor, setBgColor] = useState();
-  const [colorText, setcClorText] = useState(bgColor);
+  
 
   const changeColor = () => {
-    setBgColor("purple");
-  };
-  const onTextAreaChange = (e) => {
-    setcClorText(e.target.value);
+    setBgColor(userColor);
   };
 
   return (
     <div>
-      <input
-        placeholder="Escribe un color aquí"
-        onChange={onTextAreaChange}
-        className="input_style"
-      />
       <div
         className={`shape ${type}`}
         onClick={changeColor}
-        style={{ backgroundColor: colorText }}
+        style={{ backgroundColor: bgColor }}
       ></div>
     </div>
   );
--- games/gift-game/my-gift-app/src/pages/paint/Paint.jsx
+++ games/gift-game/my-gift-app/src/pages/paint/Paint.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import Shape from "../../components/shape/Shape";
-import './_Paint.css';
+import './_Paint.css'
 
 const Paint = () => {
+  const [userColor, setUserColor] = useState('');
+
+  const onTextAreaChange = (userColor) => {
+    setUserColor(userColor.target.value);
+  };
+  const changeButtonColor = (color) => {
+    setUserColor(color);
+  };
+
   return (
     <div>
-      
-      <Shape type="circle"/>
-      <Shape type="rectangle"/>
-      <Shape type="squre"/>
-      <Shape type="parallelogram"/>
+      <input
+        value={userColor}
+        type="text"
+        placeholder="Escribe un color aquí"
+        onChange={onTextAreaChange}
+      />
+      <div className="colorBox red" onClick={() => changeButtonColor('red')}></div>
+      <div className="colorBox blue" onClick={() => changeButtonColor('blue')}></div>
+      <div className="colorBox black" onClick={() => changeButtonColor('black')}></div>
+
+
+      <Shape userColor={userColor} type="circle" />
+      <Shape userColor={userColor} type="rectangle" />
+      <Shape userColor={userColor} type="squre" />
+      <Shape userColor={userColor} type="parallelogram" />
     </div>
   );
 };
