2bb835c825dec9acff3510f80f05ccd40f883d9c
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-13 14:22:22 -0500

    Dos ejemplos con useState de cómo hacer que el valor del usestate se ejecute solo una vez o varias veces

--- Style-example/styles/src/pages/UseState.jsx
+++ Style-example/styles/src/pages/UseState.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 
 const UseState = () => {
-  const [count, setCount] = useState(5);
+
+  //Si quiero que el valor del useState se ejecute solo la primera vez que haya un cambio en el estado, uso una función dentro del useState
+
+/*   const [count, setCount] = useState(() => { console.log('Ejecutando una sola vez'); return 4 }); */
+
+  //Si quiero que el valor de useState se ejecute cada vez que se detecte un cambio
+  const [count, setCount] = useState(initialState());
+
+  function initialState() {
+    console.log('Ejecutándo cada vez que haya un cambio')
+    return 4
+  }
+  
   function decrement() {
     setCount(count - 1);
     setCount(count - 1);
   }
+
   function increment() {
     setCount((prevState) => prevState + 1);
     setCount((prevState) => prevState + 1);
-   
   }
 
   return (
