082feb1338e390a4128a0e8a7452b39e272e4fd5
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-13 16:48:09 -0500

    Ejemplo con useEffect y consumiendo una api

--- Style-example/styles/src/pages/UseEffect.jsx
+++ Style-example/styles/src/pages/UseEffect.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 const UseEffect = () => {
-  return (
-    <h1>Ejmplos usando useEffect</h1>
-  )
-}
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
 
-export default UseEffect
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  return (
+    <>
+      <h1>Ejmplos usando useEffect</h1>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+
+      {
+        items.map(item => {
+          return <pre>{JSON.stringify(item)}</pre>;
+        })
+      }
+    </>
+  );
+};
+
+export default UseEffect;
