28de9c41facbb8c91053da098b2e325ffcc12011
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-18 20:15:18 -0500

    fixing error

--- Assesment-I/my-assesment-1/src/App.js
+++ Assesment-I/my-assesment-1/src/App.js
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home.jsx'
-import Detail from './pages/detail/Detail.jsx'
-import About from './pages/about/About.jsx'
+import Home from './pages/home/Home.jsx';
+import Detail from './pages/detail/Detail.jsx';
+import About from './pages/about/About.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="shop">
-{/*         <Nav /> */}
+        {/*         <Nav /> */}
         <Routes>
-
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/detalle/:id" element={<Detail/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/detalle/:id" element={<Detail />} />
           <Route path="*" element={<p>404</p>} />
         </Routes>
       </div>
