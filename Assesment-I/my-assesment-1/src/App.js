50bc769b9970443c38400989682dd7a551b231ed
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-18 19:57:19 -0500

    Adding a router

--- Assesment-I/my-assesment-1/src/App.js
+++ Assesment-I/my-assesment-1/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home.jsx'
+import Detail from './pages/detail/Detail.jsx'
+import About from './pages/about/About.jsx'
+
 function App() {
   return (
-    <div className="App">
-      <h1>hla</h1>
-    </div>
+    <BrowserRouter>
+      <div className="shop">
+{/*         <Nav /> */}
+        <Routes>
+
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/detalle/:id" element={<Detail/>} />
+          <Route path="*" element={<p>404</p>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
