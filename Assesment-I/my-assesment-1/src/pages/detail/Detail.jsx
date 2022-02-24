f9a64041a97211f241b2899c90a3075e3da44972
brigitte.nikoll <roadclosed24@gmail.com>
2022-02-23 21:07:53 -0500

    Adding product-card style

--- Assesment-I/my-assesment-1/src/pages/detail/Detail.jsx
+++ Assesment-I/my-assesment-1/src/pages/detail/Detail.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './_Detail.scss';
 
-function Detail() {
-  return (
-    <div>Detail</div>
-  )
-}
+const Detail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const [flag, setFlag] = useState(false);
 
-export default Detail
+  useEffect(() => {
+    const gettingProducts = async () => {
+      const response = await axios.get(
+        `https://fakestoreapi.com/products/${id}`
+      );
+      setProduct(response.data);
+      setFlag(true);
+    };
+    gettingProducts();
+  }, [id]);
+
+  return (
+    <main className="cont">
+      {flag ? (
+        <div className="card">
+          <div className="photo">
+            <img src={product.image} alt={product.title} />
+            <p>{`⭐ ${product.rating.rate} out of 5`}</p>
+            <p>{`${product.rating.count} reviews`}</p>
+          </div>
+          <div className="description">
+            <h2>{product.title}</h2>
+            <h4>{product.category}</h4>
+            <h1>{`$ ${product.price}`}</h1>
+            <p>{product.description}</p>
+            <button>Add to Cart</button>
+            <button>Wishlist</button>
+          </div>
+        </div>
+      ) : (
+        <h1>Cargando items</h1>
+      )}
+    </main>
+  );
+};
+
+export default Detail;
