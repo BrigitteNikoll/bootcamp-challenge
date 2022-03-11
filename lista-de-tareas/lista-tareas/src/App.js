a0c99b7fd15e7e69a068c01b229249caefac4b62
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-10 21:02:12 -0500

    Lista de tareas usando useState y useEffect

---
+++ lista-de-tareas/lista-tareas/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+/* import Principal from "./pages/reducerParcial"; */
+import Principal from "./pages/principal";
+
+function App() {
+  return (
+    <div className="App">
+      <Principal />
+    </div>
+  );
+}
+
+export default App;
