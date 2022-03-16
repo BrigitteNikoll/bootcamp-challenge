bc4c83c37865e27d5e799e594a1d8524733436bc
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-16 01:13:58 -0500

    Inicio de creación de API con NodeJS usando nodemon y practica con modulos. Revisar notas CLASE 202-02

---
+++ node-base/ejemplo-modulos/init.js
@@ -0,0 +1,4 @@
+import {suma, resta} from '../ejemplo-modulos/operation.js'
+
+console.log(suma(1,5))
+console.log(resta(10,5))
---
+++ node-base/ejemplo-modulos/operation.js
@@ -0,0 +1,6 @@
+export const suma = (sum1, sum2) => {
+    return sum1 + sum2
+}
+export const resta = (sum1, sum2) => {
+    return sum1 - sum2
+}
