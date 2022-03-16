9793b658c16ff9d5b2561e8dbee7548de9ac60bc
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-16 04:40:26 -0500

    Se agregó lasa rutas raiz y /products para renderizar contenido y probar que las rutas fueron creadas. Además se hizo un console.log a request

--- api-products/server.js
+++ api-products/server.js
@@ -2,7 +2,16 @@ import express from "express";
 
 const app = express();
 
-// Launch server
+// Routes
+app.get('/', (request, response) => {
+    response.send('Hola!')
+})
+app.get('/products', (request, response) => {
+    console.log("request", request)
+    response.send("<h1>Products!<h1>");
+})
+
+// Launch server in port 5000
 app.listen(5000, () => {
     console.log("Iniatialized server!")
 });
