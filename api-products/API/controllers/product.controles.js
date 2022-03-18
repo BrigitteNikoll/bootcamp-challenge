7d5359ac0fdcecbe475cbb8fde854030e4d15e24
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-18 18:36:05 -0500

    Validación de errores para getAllProducts y para createProduct

--- api-products/API/controllers/product.controles.js
+++ api-products/API/controllers/product.controles.js
@@ -2,18 +2,25 @@ import { Product } from "../models/index.js";
 
 //Controller get all product
 export const getAllProducts = async (request, response) => {
-  const products = await Product.find();
-  response.json(products);
+  try {
+    const products = await Product.find();
+    if (products.length === 0) response.status(204).send();
+    else response.status(200).json(products);
+    response.json(products);
+  } catch (e) {
+    console.log("ERROR", e);
+    response.status(500).json({ error: e });
+  }
 };
 export const getOneProduct = async (req, res) => {
-/*   console.log('getOneProduct', req.params); */
-  const { id: idProduct } = req.params
-  const product = await Product.findById(idProduct)
+  /*   console.log('getOneProduct', req.params); */
+  const { id: idProduct } = req.params;
+  const product = await Product.findById(idProduct);
   res.json(product);
 };
 export const createProduct = async (req, res) => {
-  console.log('createProduct', req.body);
+  /*   console.log("createProduct", req.body); */
   const product = new Product(req.body);
-  const newProduct = await product.save()
-  res.json(newProduct)
+  const newProduct = await product.save();
+  newProduct && res.status(201).json(newProduct);
 };
