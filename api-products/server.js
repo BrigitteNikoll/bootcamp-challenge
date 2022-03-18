0ce946b0fde5b33f179a23c008e7b20a6c727cfb
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-18 07:06:54 -0500

    Clase 04-02: Se reestructuro el código por un error. Ahora este código nos debe devolver el array de productos en POSTMAN

--- api-products/server.js
+++ api-products/server.js
@@ -1,48 +1,49 @@
 import express from "express";
 import mongoose from "mongoose";
 
-
 /* MONGOSSE */
 
-const { Schema } = mongoose;
-
-const connectDB = async () => {
-  const connect = await mongoose.connect(
-    "mongodb+srv://brigitte:root@clase-02-02.u6sw6.mongodb.net/product_db"
-  );
-
-  //Schema para productos
-  const schemaProducts = new mongoose.Schema(
-    {
-      ref: String,
-      name: String,
-      description: String,
-    },
-    { collection: "product" }
-  );
-
-  //Esto ya sería un modelo que va a resolver el schema
-  const Product = mongoose.model("Product", schemaProducts, "product");
-  const products = await Product.find();
-  console.log("PRODUCTS", products);
+//Schema para productos
+const schemaProducts = {
+  ref: String,
+  name: String,
+  description: String,
 };
 
-connectDB();
+//Product model
+const Product = mongoose.model("Product", schemaProducts, "product");
+
+//Connect to db
+await mongoose.connect(
+  "mongodb+srv://brigitte:root@clase-02-02.u6sw6.mongodb.net/product_db"
+);
+
+//Listener to connection error
+mongoose.connection.on("error", function (e) {
+  console.log("error", e);
+});
+
+
+
+//Controller get all product
+const getAllProducts = async (request, response) => {
+  const products = await Product.find();
+  response.json(products)
+};
 
 /* EXPRESS */
 const app = express();
 
+
 // Routes
 app.get("/", (request, response) => {
-  response.send("Hola!");
+  response.send("API PRODUCTS");
 });
-app.get("/products", (request, response) => {
-  /*     console.log("request", request) */
-  console.log("request", request.query);
-  response.send("<h1>Products!<h1>");
-});
+app.get("/products", getAllProducts);
+
 
 // Launch server in port 5000
 app.listen(5000, () => {
   console.log("Iniatialized server!");
 });
+
