a0826a8ad836318675d6af16b77fd7c905c0fd13
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-19 15:56:31 -0500

    Get, Create y Update ok

--- api-products/API/controllers/product.controller.js
+++ api-products/API/controllers/product.controller.js
@@ -2,8 +2,13 @@ import { Product } from "../models/index.js";
 
 // Controller get all products
 export const getAllProducts = async (request, response) => {
-  const products = await Product.find();
-  response.json(products);
+  try {
+    const products = await Product.find();
+    if (products.length === 0) response.status(204).send();
+    else response.status(200).json(products);
+  } catch (e) {
+    response.status(500).json({ error: e });
+  }
 };
 
 export const getOneProduct = async (req, res) => {
@@ -13,7 +18,45 @@ export const getOneProduct = async (req, res) => {
 };
 
 export const createProduct = async (req, res) => {
-  const product = new Product(req.body);
-  const newProduct = await product.save();
-  res.json(newProduct);
+  try {
+    const product = new Product(req.body);
+    const newProduct = await product.save();
+    newProduct && res.status(201).json(newProduct);
+  } catch (e) {
+    response.status(500).json({ error: e });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id: idProduct } = req.params;
+  const productToUpdate = req.body;
+  console.log("productToUpdate", productToUpdate);
+  const product = await Product.findById(idProduct);
+  console.log("product", product);
+
+  try {
+    Product.updateOne(product, productToUpdate, (error, updatedProduct) => {
+      if (!error) {
+        res.status(200).json(updatedProduct);
+      } else res.status(500).send(error);
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+//Probando si esto funciona
+  
+/*   const { id: idProduct } = req.params;
+  const productToUpdate = req.body;
+
+  console.log("productToUpdate", productToUpdate);
+
+  const product = await Product.findById(idProduct);
+
+  console.log("product", product);
+
+  Product.updateOne(product, productToUpdate, (err, data) => {
+    console.log("Callback", err, data);
+  }); */
+
 };
---
+++ api-products/API/routes/product.router.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import { productCtrl } from "../controllers/index.js";
+const {
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+  updateProduct,
+} = productCtrl;
+
+const router = express.Router();
+
+const productRoutes = {
+  GET_ALL: "/products/",
+  GET_ONE: "/products/:id",
+  CREATE: "/products/create",
+  UPDATE: "/products/update/:id",
+/*   DELETE: "/products/delete/:id", */
+};
+
+router.get(productRoutes.GET_ALL, getAllProducts);
+router.get(productRoutes.GET_ONE, getOneProduct);
+router.post(productRoutes.CREATE, createProduct);
+router.put(productRoutes.UPDATE, updateProduct);
+/* router.delete(productRoutes.DELETE, deleteProduct); */
+
+export default router;
