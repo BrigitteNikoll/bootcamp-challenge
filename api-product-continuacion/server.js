502d147d500d4387ec42d30c78dab3421d7fe067
brigitte.nikoll <roadclosed24@gmail.com>
2022-03-18 23:40:50 -0500

    Copia que devuelve un array vacio

---
+++ api-product-continuacion/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import { productCtlr } from "./api/controllers/index.js";
+
+const { getAllProducts, getOneProduct, createProduct } = productCtlr;
+
+/**
+ * Mongoose
+ */
+
+//Connect to db
+await mongoose.connect(
+  "mongodb+srv://brigitte:root@clase-02-02.u6sw6.mongodb.net/product_db"
+);
+
+// Listener to connection error
+mongoose.connection.on("error", function (e) {
+  console.error("ERROR: ", e);
+});
+
+/**
+ * Express
+ */
+const app = express();
+ 
+//Creación de Middleware
+app.use(express.json())
+
+// Routes
+app.get("/", (request, response) => {
+  response.send("API PRODUCTS");
+});
+
+app.get("/api/products", getAllProducts);
+app.get("/api/products/:id", getOneProduct);
+app.post("/api/products/create", createProduct);
+
+// Launch server in port 5000
+app.listen(5000, () => {
+  console.log("Iniatialized server!");
+});
+
